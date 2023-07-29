ac469fc1f21b699a7d30730d8a51dbea8f1110ab
Raviraj patil <ravirajpatil298@gmail.com>
2023-07-29 17:49:28 +0530

    add docker

---
+++ example3/Add.java
@@ -0,0 +1,15 @@
+
+public class Add {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int sum;
+        sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void main1(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ example4/Sub.java
@@ -0,0 +1,9 @@
+
+public class Sub {
+    public static void main(String args[]) {
+        int a = 30;
+        int b = 10;
+        int sub = a - b;
+        System.out.println(sub);
+    }
+}
