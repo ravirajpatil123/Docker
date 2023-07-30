b6b25861e4dc5a5fffc18a3b24445bb6621307c2
Raviraj patil <ravirajpatil298@gmail.com>
2023-07-30 20:05:32 +0530

    division

---
+++ example7/Div.java
@@ -0,0 +1,13 @@
+//package example5;
+public class Div {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 20;
+
+        int div;
+        div = a / b;
+        System.out.println(div);
+    }
+
+   
+}
