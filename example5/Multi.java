2a9a9173a8e5fa466e3d4dfc0f9bd70f2dab4f06
Raviraj patil <ravirajpatil298@gmail.com>
2023-07-30 16:09:05 +0530

    example

---
+++ example5/Multi.java
@@ -0,0 +1,15 @@
+//package example5;
+public class Multi {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int multi;
+        multi = a * b;
+        System.out.println(multi);
+    }
+
+    public static void main1(String[] args) {
+        System.out.println("Hello World");
+    }
+}
